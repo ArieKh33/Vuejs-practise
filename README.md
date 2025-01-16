c8efbe9c389c83500d10a237dc1a12c8dc870a35
ArieKh33 <akharamany04@gmail.com>
2025-01-16 14:54:07 +0100

    Changed the meta tags, added 2 new pictures for the main menu and editied the readme

--- README.md
+++ README.md
@@ -6,3 +6,18 @@ This project is made to practise getting used to using vuejs.
 ## Why have I made this?
 
 I thought of a more fun way to learn or understand new techniques, and making a small library of what I'm capable of is really fun to me.
+
+
+
+### What features does it currently have and what will I see in the future?
+
+
+#### What currently works
+1. Receiving date from api's
+2. Editing values with ref() in a page
+
+
+#### What will be added in the future
+1. Crud functionality with a JSON file.
+2. Making a tic tac toe game.
+3. More to come 
--- practise-1/src/views/MenuView.vue
+++ practise-1/src/views/MenuView.vue
@@ -1,11 +1,18 @@
 <script setup>
 
 import { RouterLink } from 'vue-router';
-import TestImg from '../assets/img/testpic.avif'
+import TestImg from '../assets/img/testpic.avif';
+import ApiImg from '../assets/img/apiMenuPicture.avif';
+import CrudImg from '../assets/img/crudMenuPicture.avif';
+
 import 'animate.css';
 import { ref } from 'vue'
 
 const project1hover = ref(false)
+const project2hover = ref(false)
+const project3hover = ref(false)
+const project4hover = ref(false)
+
 
 </script>
 
@@ -16,15 +23,20 @@ const project1hover = ref(false)
      <h1 class="text-5xl mb-4 border-b border-black">The mini projects</h1>
      <div class="flex flex-col">
       <RouterLink class="text-4xl mb-2 rounded p-1 bg-slate-200 hover:text-white hover:bg-blue-950" @mouseenter="project1hover = true" @mouseleave="project1hover = false" to="/project1">1. Using api's</RouterLink>
-      <RouterLink class="text-4xl mb-2 rounded p-1 bg-slate-200 hover:text-white hover:bg-blue-950" @mouseenter="project1hover = true" @mouseleave="project1hover = false" to="/project2">2. Getting familiar with ref</RouterLink>
-      <RouterLink class="text-4xl mb-2 rounded p-1 bg-slate-200 hover:text-white hover:bg-blue-950" @mouseenter="project1hover = true" @mouseleave="project1hover = false" to="/project3">3. CRUD with JSON</RouterLink>
-      <RouterLink class="text-4xl mb-2 rounded p-1 bg-slate-200 hover:text-white hover:bg-blue-950" @mouseenter="project1hover = true" @mouseleave="project1hover = false" to="/">4. PLACEHOLDER</RouterLink>
+      <RouterLink class="text-4xl mb-2 rounded p-1 bg-slate-200 hover:text-white hover:bg-blue-950" @mouseenter="project2hover = true" @mouseleave="project2hover = false" to="/project2">2. Getting familiar with ref</RouterLink>
+      <RouterLink class="text-4xl mb-2 rounded p-1 bg-slate-200 hover:text-white hover:bg-blue-950" @mouseenter="project3hover = true" @mouseleave="project3hover = false" to="/project3">3. CRUD with JSON</RouterLink>
+      <RouterLink class="text-4xl mb-2 rounded p-1 bg-slate-200 hover:text-white hover:bg-blue-950" @mouseenter="project4hover = true" @mouseleave="project4hover = false" to="/">4. PLACEHOLDER</RouterLink>
      </div>
     </section>
 
     <section>
       <h1 class="text-5xl border-b border-black">Preview</h1>
-      <img class="animate__animated animate__slideInRight w-100 border border-black" v-if="project1hover" id="project1img" :src=TestImg alt="">
+      <img class="animate__animated animate__slideInRight w-100 border border-black" v-if="project1hover" id="project1img" :src=ApiImg alt="api menu picture">
+      <img class="animate__animated animate__slideInRight w-100 border border-black" v-if="project2hover" id="project2img" :src=CrudImg alt="A visual example of crud">
+      <img class="animate__animated animate__slideInRight w-100 border border-black" v-if="project3hover" id="project3img" :src=TestImg alt="grass picture">
+      <img class="animate__animated animate__slideInRight w-100 border border-black" v-if="project3hover" id="project3img" :src=TestImg alt="grass picture">
+
+
     </section>
   </article>
   
